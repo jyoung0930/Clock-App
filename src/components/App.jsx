6310ffedd7405e410e773459ad18078e3924487f
Jaron Young <jaronyoung1@gmail.com>
2023-03-22 20:38:42 -0500

    Making an effort to make the layout more responsive. Got the data to generate random quote

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,25 @@ import Time from "./Time";
 import Info from "./Info";
 
 export default function App() {
+  const [quote, setQuote] = useState({});
+
+  // Generate quote
+  useEffect(() => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote({
+          content: data.content,
+          author: data.author,
+        });
+      });
+  }, []);
+
+  console.log(quote.content);
+  console.log(quote.author);
   return (
     <div>
-      <Time />
+      <Time quoteData={quote} />
     </div>
   );
 }
--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -3,34 +3,34 @@ import sunIcon from "../assets/desktop/icon-sun.svg";
 import downArrow from "../assets/desktop/icon-arrow-down.svg";
 import upArrow from "../assets/desktop/icon-arrow-up.svg";
 
-export default function Time() {
+export default function Time({ quoteData }) {
   return (
     <main className="time_section">
-      <section className="quote">
-        <p className="quote_heading">
-          “The science of operations, as derived from mathematics more
-          especially, is a science of itself, and has its own abstract truth and
-          value.”
-        </p>
-        <p className="quote_author">Ada Lovelace</p>
-      </section>
-      <section className="time_button_section">
-        <div className="time_info">
-          <p className="time_greeting">
-            <img src={moonIcon} alt="weather icon" /> GOOD EVENING, IT'S
-            CURRENTLY
-          </p>
-          <p className="time">23:14</p>
-          <p className="country">IN LONDON, UK</p>
-        </div>
-
-        <div className="button">
-          <p>MORE</p>
-          <div className="arrow_container">
-            <img src={downArrow} alt="arrow" />
+      <div className="time_padding">
+        <section className="quote">
+          <p className="quote_heading">"{quoteData.content}"</p>
+          <p className="quote_author">{quoteData.author}</p>
+        </section>
+        <section className="time_button_section">
+          <div className="time_info">
+            <p className="time_greeting">
+              <img src={moonIcon} alt="weather icon" /> GOOD EVENING, IT'S
+              CURRENTLY
+            </p>
+            <p className="time">23:14</p>
+            <p className="country">IN LONDON, UK</p>
           </div>
-        </div>
-      </section>
+
+          <div className="button">
+            <p>MORE</p>
+            <div className="arrow_container">
+              <img src={downArrow} alt="arrow" />
+            </div>
+          </div>
+        </section>
+      </div>
+      {/* conditionally render info box */}
+      {/* <div className="info-box"></div> */}
     </main>
   );
 }
